3db97062c36e1be83598df144cca5bc20d243abb
Ben Guaragno <ben.guaragno@rutgers.edu>
2020-05-12 22:42:13 -0400

    Set up demo image view

--- Custom Photo Frame/src/com/voidStudios/photoDisplay/Main.java
+++ Custom Photo Frame/src/com/voidStudios/photoDisplay/Main.java
@@ -1,26 +1,38 @@
 package com.voidStudios.photoDisplay;
-	
+
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
-
+import javafx.geometry.Rectangle2D;
 
 public class Main extends Application {
+
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,400,400);
+			BorderPane root=(BorderPane) FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene=new Scene(root, 1920, 1080);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			boolean debug=true;
+			if(!debug) {
+				ObservableList<Screen> screens=Screen.getScreens();
+				Rectangle2D bounds=screens.get(1).getVisualBounds();
+				primaryStage.setX(bounds.getMinX());
+				primaryStage.setY(bounds.getMinY());
+				primaryStage.setFullScreen(true);
+			}
 			primaryStage.show();
-		} catch(Exception e) {
+		}catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
--- Custom Photo Frame/src/com/voidStudios/photoDisplay/MainController.java
+++ Custom Photo Frame/src/com/voidStudios/photoDisplay/MainController.java
@@ -1,5 +1,17 @@
 package com.voidStudios.photoDisplay;
 
+import java.io.File;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
 public class MainController {
+
+	@FXML
+	private ImageView imageViewer;
 	
+	public void initialize() {
+		imageViewer.setImage(new Image(new File("photos/space.jpg").toURI().toString()));
+	}
 }
